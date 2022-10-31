8deb5ceae0a29d73b704695c864a2a8b2d14a7cc
fatouk2 <113057939+fatouk2@users.noreply.github.com>
2022-10-30 22:49:35 -0400

    alot of progress is being made but I'm not satisfied on how my fishes are moving. right now i want to focus on the scenary of the ocean

--- midterm.js
+++ midterm.js
@@ -1,112 +1,116 @@
-let x,y;
-let fish1;
-let fish2;
 let bubble;
-let fishArray = [];
-let fish2Array = [];
 let bubbleArray = [];
-
-function setup(){
-  createCanvas(800,800);
-  //create fish
-  for(i = 0; i<= 3; i++){  
-   fishArray.push (new Fish(50*i,200+50*i,[random(99,174),random(217,240),random(103,107)])); 
-  }
-  
-  for(i = 0; i<=3; i++){
-    fish2Array.push(new Fish(750,200+50*i, 255));
-  }
-  // print(fish1.x,fish1.y)
- //bubble = new Bubble();
-  for(i = 0; i<=3; i++){
-    bubbleArray.push(new Bubble(random(10,700), random(10,700)));
-    //bubbleArray.push(new Bubble(createVector(100*i, 200)));
-  }
+let fishes;
+let fish = [];
+let c = 25;
+function setup() {
+  createCanvas(800, 800);
+ // bubble = new Bubble();
+  for(i = 0; i < 10; i++){
+    fish[i] = (new Fish(20*i,300));
+  fish.push(new Fish((i),[random(31,202),random(132,255),random(6,255)]));
+}
+   for(i = 0; i < 30; i++){
+  bubbleArray.push(new Bubble(i));
+}
   
 }
 
-function draw(){
-background(67,213,246);
-  // fish1.move();
-  // fish1.display();
+function draw() {
+  background(21,171,255);
+  fill(255);
+  sand();
   
-//  fishArray[0].move();
-//  fishArray[0].display(64,219,76);
-//  fishArray[1].move();
- // fishArray[1].display();
- // fishArray[2].move();
- // fishArray[2].display();
- // fish2Array[0].moveOpposite();
-//  fish2Array[0].display();
- // fish2Array[1].moveOpposite();
- // fish2Array[1].display();
-   for (i = 0; i<= 3; i++) {
-     fishArray[i].move();
-     fishArray[i].display();
-   }
-  for(i = 0; i<=3; i++){
-    fish2Array[i].moveOpposite();
-    fish2Array[i].display();   
+//  bubble.display();
+  //bubble.move();
+  //bubble.update();
+  for(j = 0; j < 10; j++){
+    fish[j].display();
+     fish[j].move();
+     fish[j].update();
   }
-  
-  for(i =0; i<=200; i+=10){
-    bubbleArray[i].update();
+ for(i = 0; i < 30; i++){
     bubbleArray[i].display();
-    
+     bubbleArray[i].move();
+     bubbleArray[i].update();
   }
   
- // bubble1.move();
-  //bubble1.display();
- }
-
- class Fish{
-constructor(x,y,fishcolor){
-  this.x = x;
-  this.y = y;
-  this.fishcolor = fishcolor;
 }
-   move(){
-     this.x = this.x + 3;      
-   }
-   moveOpposite(){
-     this.x = this.x - 3;  
-   }
 
-  display(){
-    fill(this.fishcolor);
-    ellipse(this.x,this.y,60,20);
-  }
+function sand(){
+  strokeWeight(5);
+  stroke(255,231,100,70)
+  fill(255,231,100);
+   ellipse(550,550,500,200);  // sand
+  ellipse(150,550,500,200);  //sand  
+}
 
- }
+function seatree(){
+  
+  
+  
+}
 
-class Bubble{
-  constructor(x,y){
-    this.position = new createVector(700,50);
+class Fish {
+  constructor(fishcolor){
+  this.position = new createVector(random(width),random(height));
+  //  this.position = new createVector(100,600);
+    
     this.velocity = new createVector(1,2);
-   // this.speed = 1;
-   // this.pos = pos;
+    //this.size = random(5,20);
+    
+    this.fishcolor = fishcolor;
   }
-  update(){
+  move(){
     this.position.add(this.velocity);
     
-    if((this.position.x > width) || (this.position.x < 0)){
-      this.velocity.x = this.velocity.x*-1;
+  }
+  update(){
+    if(this.position.x > width ){
+      this.position.x = 0;
+     //this.velocity.x = this.velocity.x *-1 
     }
-    if((this.position.y > height) || (this.position.y < 0)){
-      this.velocity.y = this.velocity.y * -1;
+    if (this.position.y > 300){
+      this.position.y = 0;
+      //this.velocity.y = this.velocity.y *-1
     }
   }
-  //move(){
-   // this.pos.y = this.pos.y + 5; 
- // }
+  
   display(){
-    stroke(255);
     strokeWeight(2);
-    fill(148,233,250);
-    //ellipse(x,y,10,10);
-    ellipse(this.position.x,this.position.y,10,10);
+    stroke(255);
+    fill(this.fishcolor);
+    
+    ellipse(this.position.x, this.position.y, 40,10);
   }
   
+}
+
+class Bubble {
+  constructor(){
+    this.position = new createVector(random(0,500),random(0,500));
+    this.velocity = new createVector(1,2);
+    this.size = random(5,20);
+  }
+  move(){
+    this.position.add(this.velocity);
+    
+  }
+  update(){
+    if (this.position.y >= 500){
+      this.velocity.y = this.velocity.y *-1
+    }
+    if(this.position.y <= 0){
+      this.position.y = 500;
+      this.position.x = random(0,500);
+    }
+  }
   
+  display (){
+    strokeWeight(2);
+    stroke(255);
+    fill(126,255,246);
+    circle(this.position.x, this.position.y, this.size);
+  }
   
 }
