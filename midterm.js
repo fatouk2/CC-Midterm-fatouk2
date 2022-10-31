108c2f121ef29c0e3018df40abe0e9886b746506
fatouk2 <113057939+fatouk2@users.noreply.github.com>
2022-10-31 13:40:19 -0400

    began my second scene. It's not really looking how I want it too look but it's okay

--- midterm.js
+++ midterm.js
@@ -1,24 +1,35 @@
 let x = 500;
 let y = 500;
+let count;
+let current;
 let bubble;
 let bubbleArray = [];
 let fishes;
 let fish = [];
+let waterArray = [];
 let c = 25;
 function setup() {
+  count = 2000;
+  current = 0;
   createCanvas(800, 800);
  // bubble = new Bubble();
+  
   for(i = 0; i < 10; i++){
     fish[i] = (new Fish(20*i,300));
-  fish.push(new Fish((i),[random(31,202),random(132,255),random(6,255)]));
+  fish.push(new Fish(random(31,202),random(132,255),random(6,255)));
 }
    for(i = 0; i < 30; i++){
   bubbleArray.push(new Bubble(i));
 }
+  for(i = 0; i < 10; i++){
+    waterArray.push(new Water(i));
+  }
+  
   
 }
 
 function draw() {
+  if(frameCount < 500){
   background(21,171,255);
   fill(255);
   sand();
@@ -37,6 +48,18 @@ function draw() {
     bubbleArray[i].display();
      bubbleArray[i].move();
      bubbleArray[i].update();
+  }
+  }
+  if(frameCount > 500 && frameCount < 1000){
+    background(255);
+    fill(99,192,255);
+    rect(0,300,width,400);
+    for(i = 0; i<10; i ++){
+      waterArray[i].display();
+      waterArray[i].move();
+      waterArray[i].update();
+    }
+    
   }
   
 }
@@ -84,6 +107,7 @@ class Fish {
   display(){
     strokeWeight(2);
     stroke(255);
+   //fill ( random(31,202),random(132,255),random(6,255));
     fill(this.fishcolor);
     
     ellipse(this.position.x, this.position.y, 40,10);
@@ -118,4 +142,29 @@ class Bubble {
     circle(this.position.x, this.position.y, this.size);
   }
   
+}
+
+class Water{
+  constructor(){
+  this.position = new createVector(random(width),random(500,800));
+    this.position.x = random(width);
+    this.position.y = random(300,800);
+    this.velocity = new createVector(1);
+  }
+  move(){
+    this.position.add(this.velocity);
+  }
+  update(){
+    if(this.position.x > width ){
+      this.position.x = 0;
+    }
+  }
+  display(){
+    noStroke();
+   // fill(99,192,255,10);
+    fill(81,223,255,50);
+   // line(this.position.x,this.position.y, this.position.x+100,this.position.y+100);
+    rect(this.position.x,this.position.y,100,30);
+   // ellipse(this.position.x,this.position.y,100,30);
+  }
 }
