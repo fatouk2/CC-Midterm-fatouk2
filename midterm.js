68f1e8dd7282fbd1f6c142bc33016c894ac20501
fatouk2 <113057939+fatouk2@users.noreply.github.com>
2022-11-14 03:12:56 -0500

    Im sorry for the lateness it took me a while to redownload everything since I just got my laptop back

--- midterm.js
+++ midterm.js
@@ -4,19 +4,28 @@ let count;
 let current;
 let bubble;
 let bubbleArray = [];
+let bubble2 = [];
 let fishes;
 let fish = [];
 let waterArray = [];
 let c = 25;
+let c1;
+let c2;
+let weeds=[];
 function setup() {
+ 
   count = 2000;
   current = 0;
   createCanvas(800, 800);
  // bubble = new Bubble();
-  
+  background(255);
+   c1 = color(162,253,255);
+  c2 = color(48,130,255);
+ for(i= 0; i< 60; i++){
+   weeds.push(new seaWeed(i));
+ }
   for(i = 0; i < 10; i++){
-    fish[i] = (new Fish(20*i,300));
-  fish.push(new Fish(random(31,202),random(132,255),random(6,255)));
+  fish.push(new Fish([random(31,202),random(132,255),random(6,255)]));
 }
    for(i = 0; i < 30; i++){
   bubbleArray.push(new Bubble(i));
@@ -25,20 +34,23 @@ function setup() {
     waterArray.push(new Water(i));
   }
   
+  for(i = 0; i<10;i++){
+    bubble2.push(new Bubble2(i));
+  }
   
 }
 
+
 function draw() {
   if(frameCount < 500){
-  background(21,171,255);
-  fill(255);
-  sand();
-  for(x = 100; x<=width; x+=200){
-  seatree(x,550);
-  }
-//  bubble.display();
-  //bubble.move();
-  //bubble.update();
+ // background(21,171,255);
+  //fill(255);
+   // background(0);
+ rect1();
+ 
+for(j=0;j<60;j++){
+  weeds[j].show();
+}
   for(j = 0; j < 10; j++){
     fish[j].display();
      fish[j].move();
@@ -50,6 +62,13 @@ function draw() {
      bubbleArray[i].update();
   }
   }
+  function rect1(){
+  noStroke();
+  for(i = 0;i<=799;i++){
+    fill(lerpColor(c1,c2,i/799)); 
+    rect(0,0+i,800,i);
+  }
+}
   if(frameCount > 500 && frameCount < 1000){
     background(255);
     fill(99,192,255);
@@ -59,23 +78,21 @@ function draw() {
       waterArray[i].move();
       waterArray[i].update();
     }
+    for(i = 0; i<10; i ++){
+      waterArray[i].display();
+      waterArray[i].update();
+    }
     
   }
   
 }
 
-function sand(){
-  strokeWeight(5);
-  stroke(250,191,97,70)
-  fill(250,191,97);
-   ellipse(550,550,500,200);  // sand
-  ellipse(150,550,500,200);  //sand  
-}
 
-function seatree(x,y){
-  fill(255);
-  ellipse(x,y-50,30,100);
-  ellipse(x+30,y-50,50,20);
+function fishs(x,y){
+  ellipse(x,y, 70,30);
+  triangle(x-35,y,x-65,y+20,x-65,y-20);
+  fill(0);
+  circle(x+30,y-2,5);
 }
 
 class Fish {
@@ -102,15 +119,16 @@ class Fish {
      this.position.y = random(100,300);
       //this.velocity.y = this.velocity.y *-1
     }
+    
+    
   }
   
   display(){
-    strokeWeight(2);
-    stroke(255);
+    noStroke();
    //fill ( random(31,202),random(132,255),random(6,255));
     fill(this.fishcolor);
-    
-    ellipse(this.position.x, this.position.y, 40,10);
+   // fill(random(31,202),random(132,255),random(6,255));
+    fishs(this.position.x,this.position.y);
   }
   
 }
@@ -119,7 +137,7 @@ class Bubble {
   constructor(){
     this.position = new createVector(random(0,500),random(0,500));
     this.velocity = new createVector(1,2);
-    this.size = random(5,20);
+    this.size = random(45,70);
   }
   move(){
     this.position.add(this.velocity);
@@ -138,7 +156,7 @@ class Bubble {
   display (){
     strokeWeight(2);
     stroke(255);
-    fill(126,255,246);
+    fill(126,255,246,50);
     circle(this.position.x, this.position.y, this.size);
   }
   
@@ -168,3 +186,51 @@ class Water{
    // ellipse(this.position.x,this.position.y,100,30);
   }
 }
+
+class seaWeed{
+  constructor(){
+    this.x = random(0,width);
+    this.y = random(300,500);
+    this.width = random(10,15);
+    this.height = 300;
+    this.curve = 20;
+    this.color = ([random(9,174),random(121,255),random(105,175)]);
+  }
+  show(){
+    fill(this.color);
+    rect(this.x,this.y,this.width,this.height,this.curve);
+  }
+  
+  
+}
+
+class Bubble2 {
+  constructor(){
+    this.position = new createVector(random(0,500),random(0,500));
+    this.velocity = new createVector(1,2);
+    this.size = random(50,80);
+  }
+ // move(){
+ //   this.position.add(this.velocity);
+    
+ // }
+  update(){
+    if (this.position.y >= 500){
+      this.velocity.y = this.velocity.y *-1
+    }
+    if(this.position.y <= 0){
+      this.position.y = 500;
+      this.position.x = random(0,500);
+    }
+   
+  }
+  
+  display (){
+    strokeWeight(2);
+    stroke(255);
+    fill(126,255,246);
+    circle(this.position.x, this.position.y, this.size);
+  }
+  
+}
+
